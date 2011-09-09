c131f3e2f5e1bd696fcc5cfaf491f3966f4163cd
Zylex <zylex.stk@gmail.com>
2011-09-09 10:04:50 +0200

    i cant see if there is anyhting left, but it should at least work to some extent. we need to work on server to be able to test.

--- src/dk/knord/chat/client/ChatServerInput.java
+++ src/dk/knord/chat/client/ChatServerInput.java
@@ -5,12 +5,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
 import java.util.Vector;
 
-import com.sun.corba.se.spi.activation.Server;
-
+import dk.knord.chat.client.KNordHeaderFields.ClientToServer;
 import dk.knord.chat.client.KNordHeaderFields.ServerToClient;
 
 /**
@@ -33,55 +30,38 @@ public class ChatServerInput implements Runnable {
 					String message = input.readLine();
 					if (message.equals("")) {
 						// execute the messageBuffer
-					}
-					else {
-						//add message to buffer
+						executeMessageBuffer();
+					} else {
+						// add message to buffer
 						inputMessageBuffer.add(message);
 					}
-				
-				
-				/*
-				// TODO next line problem if a bunch of new lines
-				if (input.ready()) {
-					StringTokenizer st = new StringTokenizer(input.readLine());
 
-					switch (ServerToClient.getCommand(st.nextToken())) {
-					case ServerToClient.DISCONNECT:
-						ChatClient.disconnect();
-						break;
-					case ServerToClient.LIST:
-						Vector<String> chatters = new Vector<String>(0);
-						for (int i = 0; i < st.countTokens(); i++) {
-							String chatter = st.nextToken();
-							if (chatter.isEmpty())
-								continue;
-
-							chatters.add(chatter);
-						}
-						Collections.sort(chatters);
-						ChatClient.listChatters(chatters);
-						break;
-					case ServerToClient.MESSAGE:
-						st.nextToken();
-						String msg = st.nextToken() + " says: ";
-						while (st.hasMoreTokens()) {
-							msg += st.nextToken();
-						}
-						ChatClient.printMsg(msg);
-
-						// how do you know if its a 1to1 or a brodcast?
-						// protocol does not allow it...
-						
-						break;
-					case ServerToClient.NO_SUCH_ALIAS:
-						ChatClient.noSuchAlias();
-						break;
-					case ServerToClient.UNSUPPORTED:
-						ChatClient.unsupported();
-						break;
-					default:
-						ChatClient.unknown();
-					}*/
+					/*
+					 * // TODO next line problem if a bunch of new lines if
+					 * (input.ready()) { StringTokenizer st = new
+					 * StringTokenizer(input.readLine());
+					 * 
+					 * switch (ServerToClient.getCommand(st.nextToken())) { case
+					 * ServerToClient.DISCONNECT: ChatClient.disconnect();
+					 * break; case ServerToClient.LIST: Vector<String> chatters
+					 * = new Vector<String>(0); for (int i = 0; i <
+					 * st.countTokens(); i++) { String chatter = st.nextToken();
+					 * if (chatter.isEmpty()) continue;
+					 * 
+					 * chatters.add(chatter); } Collections.sort(chatters);
+					 * ChatClient.listChatters(chatters); break; case
+					 * ServerToClient.MESSAGE: st.nextToken(); String msg =
+					 * st.nextToken() + " says: "; while (st.hasMoreTokens()) {
+					 * msg += st.nextToken(); } ChatClient.printMsg(msg);
+					 * 
+					 * // how do you know if its a 1to1 or a brodcast? //
+					 * protocol does not allow it...
+					 * 
+					 * break; case ServerToClient.NO_SUCH_ALIAS:
+					 * ChatClient.noSuchAlias(); break; case
+					 * ServerToClient.UNSUPPORTED: ChatClient.unsupported();
+					 * break; default: ChatClient.unknown(); }
+					 */
 				}
 			} catch (IOException e) {
 				e.printStackTrace(System.err);
@@ -99,16 +79,34 @@ public class ChatServerInput implements Runnable {
 		input = new BufferedReader(new InputStreamReader(
 				connection.getInputStream()));
 	}
-	
+
 	private void executeMessageBuffer() {
 		if (inputMessageBuffer.isEmpty())
 			ChatClient.printMsg("RECEIVED: Empty message from server.");
-			ChatClient.sendToServer("UNKNOWN");
+		ChatClient
+				.sendToServer(ClientToServer.commands[ClientToServer.UNKNOWN]);
 		String command = inputMessageBuffer.get(0);
 		switch (ServerToClient.getCommand(command)) {
+		case ServerToClient.LIST:
+			Vector<String> chatters = new Vector<String>(0);
+			for (int i = 1; i < inputMessageBuffer.size(); i++) {
+				chatters.add(inputMessageBuffer.get(i));
+			}
+			ChatClient.listChatters(chatters);
+			break;
+		case ServerToClient.DISCONNECT:
+			ChatClient.printMsg("RECEIVED:\n> DISCONNECT > Client is disconnecting...");
+			ChatClient.disconnect();
+			ChatClient.printMsg("> You are now disconnected");
+			break;
 		default:
-			// 
+			ChatClient.printMsg("RECEIVED:\n");
+			for (int index = 0; index < inputMessageBuffer.size(); index++) {
+				ChatClient
+						.printMsg("> " + inputMessageBuffer.get(index) + "\n");
+			}
 		}
+
 	}
 
 }
